2294cd7446f7ed172f2fd07ce68f9fd0a7494814
ChrisMulvany <chrismulvany@gmail.com>
2024-07-03 16:25:19 +0100

    Added models/index.js and updated server.js

---
+++ app/models/index.js
@@ -0,0 +1,17 @@
+const dbConfig = require("../config/db.config.js");
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.tutorials = require("./tutorial.model.js")(mongoose);
+
+module.exports = db;
+
+/**
+ * DON'T FORGET
+ * -------------
+ * Don't forget to call connect() method in server.js
+ */
